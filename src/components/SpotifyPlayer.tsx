36b9f8f824aff9bb1a7b0925b0b1e2452a4395b3
icefireism <randomcherry783@gmail.com>
2025-07-01 14:14:42 +0530

    Fix malformed URIs in SpotifyPlayer

--- src/components/SpotifyPlayer.tsx
+++ src/components/SpotifyPlayer.tsx
@@ -15,8 +15,8 @@ const songs: Song[] = [
     id: '1',
     title: 'her',
     artist: 'JVKE',
-    spotifyUrl: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6G9YlbU3ByPJQvOFDRdwyM?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>', // Replace with your link
-    embedUrl: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6G9YlbU3ByPJQvOFDRdwyM?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    spotifyUrl: 'https://open.spotify.com/track/6G9YlbU3ByPJQvOFDRdwyM',
+    embedUrl: 'https://open.spotify.com/embed/track/6G9YlbU3ByPJQvOFDRdwyM?utm_source=generator'
   },
   {
     id: '2',
